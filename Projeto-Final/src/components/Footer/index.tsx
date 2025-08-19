46a1cbf0c4804457c915fb77709c3961a540eab4
Henrique Trope <tropehe12@gmail.com>
2025-08-18 23:06:34 -0300

    src: add funcionalidade header e esboço do footer

--- Projeto-Final/src/components/Footer/index.tsx
+++ Projeto-Final/src/components/Footer/index.tsx
@@ -1,8 +1,64 @@
+import styles from "./styles.module.css";
+import logoInsta from "../../assets/mdi_instagram (1).png";
+import logoFace from "../../assets/ri_facebook-fill.png";
+import logoLi from "../../assets/ri_linkedin-fill.png";
+import logo from "../../assets/Logo (1).png";
+
 export default function Footer() {
-    return(
-        <>
-            <h1>Footer</h1>
-        </>
-    )
-    
+  return (
+    <>
+      <footer className={styles.footer}>
+        <div className={styles.contato}>
+          <div className={styles.endereco}>
+            <h4>Endereço</h4>
+            <ul>
+              <li>Av. Milton Tavares de Souza, 5</li>
+              <li>s/n - Sala 115 B - Boa Viagem</li>
+              <li>Niterói - RJ</li>
+              <li>CEP: 24210-315</li>
+            </ul>
+          </div>
+
+          <div className={styles.faleConosco}>
+            <h4>Fale conosco</h4>
+            <ul>
+              <li>contato@injunior.com.br</li>
+            </ul>
+          </div>
+
+          <div className={styles.redesSociais}>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+              <img src={logoInsta} alt="Instagram" />
+            </a>
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+              <img src={logoFace} alt="Facebook" />
+            </a>
+            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+              <img src={logoLi} alt="LinkedIn" />
+            </a>
+          </div>
+        </div>
+
+        <div className={styles.logoFooter}>
+          <img src={logo} alt="Logo IN Junior" />
+        </div>
+
+        <div className={styles.iFrame}>
+          <iframe
+            title="Mapa IN Junior - UFF"
+            loading="lazy"
+            allowFullScreen
+            referrerPolicy="no-referrer-when-downgrade"
+            src={
+              "https://www.google.com/maps?q=" +
+              encodeURIComponent(
+                "IN Junior - Empresa Junior de Informática, UFF - Campus Praia Vermelha, Instituto de Computação, Av. Milton Tavares de Souza, s/n - Sala 115 B - Boa Viagem, Niterói - RJ, 24210-315"
+              ) +
+              "&output=embed"
+            }
+          />
+        </div>
+      </footer>
+    </>
+  );
 }
--- Projeto-Final/src/components/Header/index.tsx
+++ Projeto-Final/src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import compras from "../../assets/Compras.png";
 import favorito from "../../assets/Favorito.png";
 import usuario from "../../assets/User.png";
@@ -7,29 +8,51 @@ import styles from "./styles.module.css";
 export default function Header() {
   return (
     <header className={styles.header}>
+      
       <div className={styles.logo}>
-        <img src={logo} alt="Logo" />
+        <Link to="/">
+          <img src={logo} alt="Logo" />
+        </Link>
       </div>
 
       <nav className={styles.nav}>
         <ul>
-          <li><a href="#">PIJAMAS</a></li>
-          <li><a href="#">FEMININO</a></li>
-          <li><a href="#">MASCULINO</a></li>
-          <li><a href="#">INFANTIL</a></li>
+          <li>
+            <Link to="/pijamas">PIJAMAS</Link>
+          </li>
+          <li>
+            <Link to="/pijamas?genero=feminino">FEMININO</Link>
+          </li>
+          <li>
+            <Link to="/pijamas?genero=masculino">MASCULINO</Link>
+          </li>
+          <li>
+            <Link to="/pijamas?genero=infantil">INFANTIL</Link>
+          </li>
         </ul>
       </nav>
 
       <div className={styles.icones}>
         <ul className={styles.iconList}>
-          <li><img src={compras} alt="Carrinho" /></li>
-          <li><img src={favorito} alt="Favoritos" /></li>
+          <li>
+            <Link to="/cart">
+              <img src={compras} alt="Carrinho" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/favoritos">
+              <img src={favorito} alt="Favoritos" />
+            </Link>
+          </li>
         </ul>
         <ul className={styles.iconList}>
-          <li><img src={usuario} alt="Usuário" /></li>
+          <li>
+            <Link to="/login">
+              <img src={usuario} alt="Usuário" />
+            </Link>
+          </li>
         </ul>
       </div>
     </header>
   );
 }
-
