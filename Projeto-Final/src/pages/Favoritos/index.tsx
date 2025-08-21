7b316985c8a1131147346b33e38ca59da7af0d61
Lucas <lucassilveira07.ls@gmail.com>
2025-08-21 11:16:58 -0300

    feat: pagina de login, feedback e cadastro

--- Projeto-Final/src/pages/Favoritos/index.tsx
+++ Projeto-Final/src/pages/Favoritos/index.tsx
@@ -1,20 +1,22 @@
-import { Link } from 'react-router-dom';
-import styles from './styles.module.css';
-import cart from '../../assets/cart.png';
-import favorite from '../../assets/favorite.png';
+import { Link } from "react-router-dom";
+import styles from "./styles.module.css";
+import cart from "../../assets/cart.png";
+import favorite from "../../assets/favorite.png";
 
 export default function favoriteoritos() {
-    return (
-        <div className={styles.background}>
-            <div className={styles.icons}>
-                <Link to="/cart">
-                    <img src={cart} />
-                </Link>
+  return (
+    <div className={styles.background}>
+      <div className={styles.top}>
+        <div className={styles.icons}>
+          <Link to="/cart">
+            <img src={cart} />
+          </Link>
 
-                <Link to="/favoritos">
-                    <img src={favorite} />
-                </Link>
-            </div>
+          <Link to="/favoritos">
+            <img src={favorite} />
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
